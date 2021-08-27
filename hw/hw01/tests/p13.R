04b7d8dd9e3707bc16560a453a7ec977ff74b829
Chandler <chandlerbeon@berkeley.edu>
2021-08-27 07:36:39 -0700

    Revert HW to Ottr 0.1.0

--- hw/hw01/tests/p13.R
+++ hw/hw01/tests/p13.R
@@ -1,32 +1,31 @@
-test = list(
-  name = "p13",
-  cases = list(
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 0.3333333333333333,
-      code = {
-        testthat::expect_true(is.data.frame(sleep_ratio), 
-                              info = "p13a: Checking sleep_ratio is a dataframe")
-      }
-    ),
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 0.3333333333333333,
-      code = {
-        testthat::expect_true(ncol(sleep_ratio) == 12, 
-                              info = "p13b: Checking sleep_time_rev has 12 collumns")
-      }
-    ),
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 0.3333333333333333,
-      code = {
-        testthat::expect_true(nrow(sleep_ratio) == 83, 
-                              info = "p13c: Checking sleep_ratio has 83 rows")
-      }
-    )
-  )
-)
+library(testthat)
+
+test_metadata = "
+cases:
+- hidden: false
+  name: p13a
+  points: 0.3333333333333333
+- hidden: false
+  name: p13b
+  points: 0.3333333333333333
+- hidden: false
+  name: p13c
+  points: 0.3333333333333333
+name: p13
+
+"
+
+test_that("p13a", {
+  expect_true(is.data.frame(sleep_ratio))
+  print("p13a: Checking sleep_ratio is a dataframe.")
+})
+
+test_that("p13b", {
+  expect_true(ncol(sleep_ratio) == 12)
+  print("p13b: Checking sleep_time_rev has 12 columns.")
+})
+
+test_that("p13c", {
+  expect_true(nrow(sleep_ratio) == 83)
+  print("p13c: Checking sleep_ratio has 83 rows.")
+})
--- hw/hw01/tests/p14.R
+++ hw/hw01/tests/p14.R
@@ -1,32 +1,31 @@
-test = list(
-  name = "p14",
-  cases = list(
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 0.3333333333333333,
-      code = {
-        testthat::expect_true(is.data.frame(sleep_r_bw), 
-                              info = "p14a: Checking sleep_r_bw is a dataframe")
-      }
-    ),
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 0.3333333333333333,
-      code = {
-        testthat::expect_true(ncol(sleep_r_bw) == 13, 
-                              info = "p14b: Checking sleep_r_bw has 13 columns")
-      }
-    ),
-    ottr::TestCase$new(
-      hidden = FALSE,
-      name = NA,
-      points = 0.3333333333333333,
-      code = {
-        testthat::expect_true(nrow(sleep_r_bw) == 83, 
-                              info = "p14c: Checking sleep_r_bw has 83 rows")
-      }
-    )
-  )
-)
+library(testthat)
+
+test_metadata = "
+cases:
+- hidden: false
+  name: p14a
+  points: 0.3333333333333333
+- hidden: false
+  name: p14b
+  points: 0.3333333333333333
+- hidden: false
+  name: p14c
+  points: 0.3333333333333333
+name: p14
+
+"
+
+test_that("p14a", {
+  expect_true(is.data.frame(sleep_r_bw))
+  print("p14a: Checking sleep_r_bw is a dataframe.")
+})
+
+test_that("p14b", {
+  expect_true(ncol(sleep_r_bw) == 13)
+  print("p14b: Checking sleep_r_bw has 13 columns.")
+})
+
+test_that("p14c", {
+  expect_true(nrow(sleep_r_bw) == 83)
+  print("p14c: Checking sleep_r_bw has 83 rows.")
+})
