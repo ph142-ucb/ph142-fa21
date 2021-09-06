b7f458c606396cb8496450236c140b39b1bb8d8b
Chandler <chandlerbeon@berkeley.edu>
2021-09-06 16:59:56 -0700

    HW02 --> Ottr 1.1.0

--- hw/hw02/tests/q11.R
+++ hw/hw02/tests/q11.R
@@ -1,23 +1,14 @@
-library(testthat)
-
-test_metadata = "
-cases:
-- hidden: false
-  name: p11a
-  points: 1
-- hidden: false
-  name: p11b
-  points: 1
-name: q11
-
-"
-
-test_that("p11a", {
-  expect_true(all.equal(CS_summary$mean_CS, 15.26642, tol = 0.01))
-  print("Checking: CS_summary has a column called `mean_CS` with the correct value")
-})
-
-test_that("p11b", {
-  expect_true(all.equal(CS_summary$median_CS, 15.6, tol = 0.01))
-  print("Checking: CS_summary has a column called `median_CS` with the correct value")
-})
+test = list(
+  name = "q11",
+  cases = list(
+    ottr::TestCase$new(
+      hidden = FALSE,
+      name = NA,
+      points = 2.0,
+      code = {
+        expect_true(all.equal(CS_summary$mean_CS, 15.26642, tol = 0.01), "Checking: CS_summary has a column called `mean_CS` with the correct value")
+        expect_true(all.equal(CS_summary$median_CS, 15.6, tol = 0.01), "Checking: CS_summary has a column called `median_CS` with the correct value")
+      }
+    )
+  )
+)
