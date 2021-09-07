2a6d10b56aea4d194df5a4b85c430f8ea3452f74
Kelsey MacCuish <kmaccuish@berkeley.edu>
2021-09-06 17:09:58 -0700

    lab02 AG updates

--- lab/lab02/tests/q15.R
+++ lab/lab02/tests/q15.R
@@ -1,47 +1,26 @@
-library(testthat)
+test = list(
+  name = "q15",
+  cases = list(
+    ottr::TestCase$new(
+      hidden = FALSE,
+      name = NA,
+      points = 1.0,
+      code = {
+        testthat::expect_true("ggplot" %in% class(p15), 
+                              info = "Checking p15 is a ggplot")
 
-test_metadata = "
-cases:
-- hidden: false
-  name: p15a
-  points: 0.2
-- hidden: false
-  name: p15b
-  points: 0.2
-- hidden: false
-  name: p15c
-  points: 0.2
-- hidden: false
-  name: p15d
-  points: 0.2
-- hidden: false
-  name: p15e
-  points: 0.2
-name: q15
+        testthat::expect_true(identical(p15$data, CS_data), 
+                              info = "p15b: Using CS_data")
 
-"
+        testthat::expect_true(rlang::quo_get_expr(p15$mapping$x) == "CS_rate_100", 
+                              info = "p15c: Checking CS_rate_100 is on the x-axis")
 
-test_that("p15a", {
-  expect_true("ggplot" %in% class(p15))
-  print("Checking: p15 is a ggplot")
-})
+        testthat::expect_true("GeomBar" %in% class(p15$layers[[1]]$geom), 
+                             info = "p15d: Made a histogram")
 
-test_that("p15b", {
-  expect_true(identical(p15$data, CS_data))
-  print("Checking: Using CS_data")
-})
-
-test_that("p15c", {
-  expect_true(rlang::quo_get_expr(p15$mapping$x) == "CS_rate_100")
-  print("Checking: CS_rate_100 is on the x axis")
-})
-
-test_that("p15d", {
-  expect_true("GeomBar" %in% class(p15$layers[[1]]$geom))
-  print("Checking: Made a histogram")
-})
-
-test_that("p15e", {
-  expect_true(length(p15$layers[[1]]$stat_params$binwidth) != 0)
-  print("Checking: Binwidth chosen")
-})
+        testthat::expect_true(length(p15$layers[[1]]$stat_params$binwidth) !=0, 
+                              info = "p15e: Checking binwdith chosen")
+      }
+    )
+  )
+)
--- lab/lab02/tests/q6.R
+++ lab/lab02/tests/q6.R
@@ -1,39 +1,24 @@
-library(testthat)
+test = list(
+  name = "q6",
+  cases = list(
+    ottr::TestCase$new(
+      hidden = FALSE,
+      name = NA,
+      points = 1.0,
+      code = {
+        testthat::expect_true("ggplot" %in% class(p6), 
+                              info = "p6a: Checking p6 is a ggplot")
 
-test_metadata = "
-cases:
-- hidden: false
-  name: p6a
-  points: 0.25
-- hidden: false
-  name: p6b
-  points: 0.25
-- hidden: false
-  name: p6c
-  points: 0.25
-- hidden: false
-  name: p6d
-  points: 0.25
-name: q6
+        testthat::expect_true(identical(p6$data, CS_data), 
+                              info = "p6b: Using CS_data")
 
-"
+        testthat::expect_true(rlang::quo_get_expr(p6$mapping$x) == "Income_Group", 
+                              info = "p6c: Checking Income_Group is on the x-axis")
 
-test_that("p6a", {
-  expect_true("ggplot" %in% class(p6))
-  print("Checking: p6 is a ggplot")
-})
+        testthat::expect_true("GeomBar" %in% class(p6$layers[[1]]$geom), 
+                              info = "p6d: Made a barchart")
 
-test_that("p6b", {
-  expect_true(identical(p6$data, CS_data))
-  print("Checking: Using CS_data")
-})
-
-test_that("p6c", {
-  expect_true(rlang::quo_get_expr(p6$mapping$x) == "Income_Group")
-  print("Checking: Income_Group is on the x axis")
-})
-
-test_that("p6d", {
-  expect_true("GeomBar" %in% class(p6$layers[[1]]$geom))
-  print("Checking: Made a barchart")
-})
+      }
+    )
+  )
+)
